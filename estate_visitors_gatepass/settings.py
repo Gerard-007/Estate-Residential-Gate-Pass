f1d1013510d1781d13e222c61c1ee6b652253d6b
Gerard-007 <gerardnwazk@gmail.com>
2025-03-09 13:50:36 +0100

    inital commit

--- estate_visitors_gatepass/settings.py
+++ estate_visitors_gatepass/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
 
     #Core Apps
     "apps.authentication",
@@ -114,7 +114,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
+# STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile', 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
